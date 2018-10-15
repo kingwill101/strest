039e66d2cb80b974cf8889cccf1c7be65a3fed62
Glenford Williams <kingwill101@localhost.localdomain>
2018-10-15 10:17:25 -0500

    add faker template functions

--- go.mod
+++ go.mod
@@ -1,9 +1,11 @@
 module gitlab.com/kingwill101/strest
 
 require (
+	github.com/corpix/uarand v0.0.0-20170903190822-2b8494104d86 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.0.0-20170109093832-22d885f9ecc7 // indirect
 	github.com/gin-gonic/gin v1.3.0
+	github.com/icrowley/fake v0.0.0-20180203215853-4178557ae428
 	github.com/json-iterator/go v1.1.5 // indirect
 	github.com/mattn/go-isatty v0.0.4 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
--- templates.go
+++ templates.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"os"
 	"text/template"
+
+	"github.com/icrowley/fake"
 )
 
 //ParseField parse fields that need access to template functions
@@ -30,7 +32,25 @@ func getTemplate() *template.Template {
 
 func tmpFuncs() template.FuncMap {
 	return template.FuncMap{
-		"ENV": getEnv,
+		"ENV":                      getEnv,
+		"MaleFirstName":            fake.MaleFirstName,
+		"FemaleFirstName":          fake.FemaleFirstName,
+		"FirstName":                fake.FirstName,
+		"MaleLastName":             fake.MaleLastName,
+		"FemaleLastName":           fake.FemaleLastName,
+		"LastName":                 fake.LastName,
+		"MalePatronymic":           fake.MalePatronymic,
+		"FemalePatronymic":         fake.FemalePatronymic,
+		"Patronymic":               fake.Patronymic,
+		"MaleFullNameWithPrefix":   fake.MaleFullNameWithPrefix,
+		"FemaleFullNameWithPrefix": fake.FemaleFullNameWithPrefix,
+		"FullNameWithPrefix":       fake.FullNameWithPrefix,
+		"MaleFullNameWithSuffix":   fake.MaleFullNameWithSuffix,
+		"FemaleFullNameWithSuffix": fake.FemaleFullNameWithSuffix,
+		"FullNameWithSuffix":       fake.FullNameWithSuffix,
+		"MaleFullName":             fake.MaleFullName,
+		"FemaleFullName":           fake.FemaleFullName,
+		"FullName":                 fake.FullName,
 	}
 }
 
