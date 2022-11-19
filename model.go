e589176c85bcc7f8e9ae93292a2dd012ba729f95
Glenford Williams <hey@glenfordwilliams.com>
2022-11-19 15:45:47 -0500

    Payload:Load parse and set environment variables in place

--- model.go
+++ model.go
@@ -12,6 +12,73 @@ type Payload struct {
 	Async   bool                `yaml:"async"`
 }
 
+func marshal[K comparable, V any](m map[K]V) (map[K]V, error) {
+
+	stringify, err := json.Marshal(m)
+
+	if err != nil {
+		return nil, err
+	}
+
+	stringify = []byte(ParseField(string(stringify)))
+	var result map[K]V
+	if err := json.Unmarshal(stringify, &result); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+
+}
+
+func (p *Payload) Load() {
+
+	for s, v := range p.Request {
+		GetLogger().Println("request", s)
+
+		for env, value := range v.Env {
+			err := os.Setenv(env, value.(string))
+			if err != nil {
+				continue
+			}
+		}
+
+		p.Request[s].URL = ParseField(v.URL)
+		p.Request[s].Method = ParseField(v.Method)
+		p.Request[s].Data.Raw = ParseField(v.Data.Raw)
+
+		if len(v.Data.Headers) != 0 {
+
+			headers, err := marshal[string, string](v.Data.Headers)
+			if err != nil {
+				continue
+			}
+			p.Request[s].Data.Headers = headers
+		}
+
+		if len(v.Data.Form) != 0 {
+
+			form, err := marshal[string, string](v.Data.Form)
+			if err != nil {
+				continue
+			}
+			p.Request[s].Data.Headers = form
+		}
+		if len(v.Data.JSON) != 0 {
+
+			requestJson, err := marshal[string, any](v.Data.JSON)
+			if err != nil {
+				continue
+			}
+			p.Request[s].Data.JSON = requestJson
+		}
+
+		for t, u := range v.Validation {
+			v.Validation[t].Body = ParseField(u.Body)
+		}
+
+	}
+}
+
 // RequestData Request data parameters
 type RequestData struct {
 	Params  map[string]interface{} `yaml:"params"`
