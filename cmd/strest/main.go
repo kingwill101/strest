7c74d5fac0b8a82fb362a972f2f48a9bc9a80980
Glenford Williams <hey@glenfordwilliams.com>
2022-11-19 17:30:21 -0500

    add cobra command

--- cmd/strest/main.go
+++ cmd/strest/main.go
@@ -1,43 +1,50 @@
 package main
 
 import (
-	"flag"
-	"time"
-
-	"github.com/sirupsen/logrus"
-
+	"github.com/spf13/cobra"
 	"gitlab.com/kingwill101/strest"
 	"gitlab.com/kingwill101/strest/runner"
 	"gitlab.com/kingwill101/strest/validators"
+	"time"
 )
-
-var script = flag.String("script", "../../test/req.yaml", "Yaml file for running validation")
-var log *logrus.Logger
-
-func init() {
-	log = strest.GetLogger()
-}
 
 func main() {
 
-	validator := validators.NewValidator()
-	validator.Register(&validators.StatusCodeValidator{})
-	validator.Register(&validators.BodyValidator{})
-	validator.Register(&validators.LogPrint{})
-	validator.Register(&validators.LogIncoming{})
+	var script string
 
-	testTime := time.Now()
+	var rootCmd = &cobra.Command{
+		Use:   "strest",
+		Short: "run http tests",
+		Long:  `Run http tests described with a predefined yaml template`,
+		Args:  cobra.MinimumNArgs(0),
+		Run: func(cmd *cobra.Command, args []string) {
 
-	p, err := strest.LoadYamlData(*script)
-	if err != nil {
-		log.Fatal(err)
+			log := strest.GetLogger()
+
+			validator := validators.NewValidator()
+			validator.Register(&validators.StatusCodeValidator{})
+			validator.Register(&validators.BodyValidator{})
+			validator.Register(&validators.LogPrint{})
+			validator.Register(&validators.LogIncoming{})
+
+			testTime := time.Now()
+
+			p, err := strest.LoadYamlData(script)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			p.Load()
+
+			runner.RunTest(validator, p)
+
+			log.Println("Completed in - ", time.Since(testTime))
+		},
 	}
+	rootCmd.Flags().StringVar(&script, "file", "../../test/req.yaml", "Path to yaml")
 
-	p.Load()
-
-	runner.RunTest(validator, p)
-
-	log.Println("Completed in - ", time.Since(testTime))
-
-	// runServer()
+	err := rootCmd.Execute()
+	if err != nil {
+		return
+	}
 }
