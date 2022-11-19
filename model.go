73e03ad137119c70d153a09a2af1ffc0fd4be106
Glenford Williams <hey@glenfordwilliams.com>
2022-11-19 15:45:09 -0500

    extend supported options

--- model.go
+++ model.go
@@ -1,12 +1,18 @@
 package strest
 
-//Payload entire yaml configuration
+import (
+	"encoding/json"
+	"os"
+)
+
+// Payload entire yaml configuration
 type Payload struct {
-	Version string             `yaml:"version"`
-	Request map[string]Request `yaml:"requests"`
+	Version string              `yaml:"version"`
+	Request map[string]*Request `yaml:"requests"`
+	Async   bool                `yaml:"async"`
 }
 
-//RequestData Request data parameters
+// RequestData Request data parameters
 type RequestData struct {
 	Params  map[string]interface{} `yaml:"params"`
 	Headers map[string]string      `yaml:"headers"`
@@ -15,22 +21,24 @@ type RequestData struct {
 	Raw     string                 `yaml:"raw"`
 }
 
-//Request Information about the current request
+// Request Information about the current request
 type Request struct {
-	FailOnError bool        `yaml:"failOnError"`
-	URL         string      `yaml:"url"`
-	Timeout     int         `yaml:"timeout"`
-	Method      string      `yaml:"method"`
-	Data        RequestData `yaml:"data"`
-	Log         bool        `yaml:"log"`
-	Validation  Validation  `yaml:"validation"`
-	Repeat      int         `yaml:"repeat"`
-	Delay       int         `yaml:"delay"`
+	FailOnError bool                   `yaml:"failOnError"`
+	URL         string                 `yaml:"url"`
+	Timeout     int                    `yaml:"timeout"`
+	Method      string                 `yaml:"method"`
+	Data        *RequestData           `yaml:"data"`
+	Log         bool                   `yaml:"log"`
+	Validation  map[string]*Validation `yaml:"validation"`
+	Repeat      int                    `yaml:"repeat"`
+	Delay       int                    `yaml:"delay"`
+	Async       bool                   `yaml:"async"`
+	Env         map[string]interface{} `yaml:"environment"`
+	DependsOn   []string               `yaml:"dependsOn"`
 }
 
-//Validation types of validation available on each request
+// Validation types of validation available on each request
 type Validation struct {
 	Body       string `yaml:"body"`
-	Status     string `yaml:"status"`
-	StatusCode int    `yaml:"statusCode"`
+	StatusCode int    `yaml:"code"`
 }
